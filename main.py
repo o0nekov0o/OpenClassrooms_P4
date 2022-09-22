90346a325393322036d971b10647774160d83fa6
o0nekov0o <33200665+o0nekov0o@users.noreply.github.com>
2022-09-23 01:35:23 +0200

    Modif depuis github
    En attendant de tester dans l'IDE
    Prochains points
    Finaliser les fonctions edit
    Faire interagir fonctions avec main_tournoi

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from modelsmvc import tournament, \
     player, round, versus, settings
-
 settings.init()
 
 main_tournoi = tournament.Tournoi.ajouter_tournoi()
@@ -11,47 +10,88 @@ def main_view():
     print("1/ Gérer les joueurs")
     print("2/ Gérer les matchs")
 
-
 def main_controller():
     main_view()
     choix = input("Que voulez-vous faire ?")
     if choix == '0':
-        pass
+        tournament_controller()
     elif choix == '1':
         player_controller()
     elif choix == '2':
-        pass
+        round_controller()
     else:
         print("Je n'ai pas compris votre choix")
         main_controller()
 
+def tournament_view():
+    print("0/ Ajouter un tournoi")
+    print("1/ Modifier un tournoi")
+    print("2/ Choisir un tournoi")
+    print("3/ Revenir au menu principal")
+        
+def tournament_controller():
+    tournament_view()
+    choix = input("Que voulez-vous faire ?")
+    if choix == '0':
+        tournament.Tournoi.ajouter_tournoi()
+        tournament_controller()
+    elif choix == '1':
+        edit_tournament_controller() # a creer
+        tournament_controller()
+    elif choix == '2':
+        choose_tournament_controller() # a creer
+        tournament_controller()
+    elif choix == '2':
+        main_controller()
+    else:
+        print("Je n'ai pas compris votre choix")
+        tournament_controller()
 
 def player_view():
-    print("0/ Ajouter un joueur")
+    print("0/ Ajouter les joueurs")
     print("1/ Modifier un joueur")
     print("2/ Revenir au menu principal")
-
 
 def player_controller():
     player_view()
     choix = input("Que voulez-vous faire ?")
     if choix == '0':
-        newplayer_controller()
+        player.Joueur.ajouter_joueur()
+        player_controller()
     elif choix == '1':
-        # selectionner tournoi, puis joueur
-        pass
+        edit_player_controller()
+        player_controller()
     elif choix == '2':
         main_controller()
     else:
         print("Je n'ai pas compris votre choix")
         player_controller()
+        
+def round_view():
+    print("0/ Ajouter les matchs")
+    print("1/ Modifier un match")
+    print("2/ Revenir au menu principal")
 
+def round_controller():
+    round_view()
+    choix = input("Que voulez-vous faire ?")
+    if choix == '0':
+        round.Tour.demarrer_tour()
+        round_controller()
+    elif choix == '1':
+        edit_round_controller() # a creer
+        round_controller()
+    elif choix == '2':
+        main_controller()
+    else:
+        print("Je n'ai pas compris votre choix")
+        round_controller()
 
+"""
 global ref
 ref = -1  # a mettre dans settings
 
-
-def newplayer_controller():
+def new_player_controller():
     global ref
     ref += 1
     nom_de_famille = input("quel est le nom du joueur ?")
@@ -65,8 +105,7 @@ def newplayer_controller():
     print("joueur ajoute")
     player_controller()
 
-
-def editplayer_controller():
+def edit_player_controller():
     for i, joueur in enumerate(main_tournoi.joueurs):
         print(f"{i}/ {joueur.nom_de_famille}")
     index = input("tapez le nom du joueur a modif")
@@ -74,6 +113,6 @@ def editplayer_controller():
     new_name = input("quel est le nouveau nom")
     player_to_modify.nom_de_famille = new_name
     main_controller()
-
+"""
 
 main_controller()
