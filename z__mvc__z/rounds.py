061b5dbb46b867d9afbff21a0a4e5046781bdf96
o0nekov0o <nydeanor@gmail.com>
2023-01-22 15:06:07 +0100

    màj recommendations PEP8 màj flake-report OK màj fonction ajouter_tour

---
+++ z__mvc__z/rounds.py
@@ -0,0 +1,192 @@
+from z__mvc__z import versus
+from z__mvc__z import settings_2
+from json2html import json2html
+import json
+import os
+
+
+class Tour:
+    def __init__(self, id, liste_de_matches):
+        self.id = id
+        self.liste_de_matches = liste_de_matches
+
+    def completed(self, main_tournoi):
+        for match in main_tournoi.tournees[-1].liste_de_matches:
+            if match.resultat is None:
+                return False
+        return True
+
+    def ajouter_tour(self, main_tournoi, bdd):
+        """
+        creates round instances, from where the associated functions to ensure the round creation will also be called
+        :param main_tournoi: from main.py, round_controller, for appending of round in the tournament rounds list
+        :return: None
+        """
+        try:
+            rounds_count = len(main_tournoi.tournees)
+            if rounds_count and not Tour(f"tour_{rounds_count + 1}", "liste_de_matchs").completed(main_tournoi):
+                print("Round en cours, finissez de le saisir")
+                Tour(f"tour_{rounds_count + 1}", "liste_de_matchs").saisir_score(main_tournoi, bdd)
+            if rounds_count == 7:
+                print("Tournoi terminé, ajoutez-en un autre")
+                with open("db.json") as file:
+                    data = json.load(file)
+                    scan_output = json2html.convert(json=data)
+                    html_report_file = "rapport.html"
+                    with open(html_report_file, "w") as html_file:
+                        html_file.write(str(scan_output))
+                        print("Rapport du tournoi généré : rapport.html")
+                os.system('python main.py')
+            number_of_rounds = len(main_tournoi.tournees)
+            id_round = f"round_{number_of_rounds + 1}"
+            new_matches = []
+            main_tournoi.joueurs.sort(key=lambda x: (x.score, x.classement), reverse=True)
+            if number_of_rounds == 0:
+                for i in range(0, 4):
+                    id_match = f"{id_round}_match_{i}"
+                    new_matches.append(versus.Match(id=id_match, liste_de_joueurs=[main_tournoi.joueurs[i],
+                                                                                   main_tournoi.joueurs[i + 4]]))
+                new_tour = Tour(id=id_round, liste_de_matches=new_matches)
+                print("Nouveau round créé")
+                print("-" * 163)
+                print("Voici la liste des matches à jouer")
+                for i, match in enumerate(new_matches):
+                    print(f"{i}/ {match.liste_de_joueurs[0].prenom} vs {match.liste_de_joueurs[1].prenom}")
+                print("-" * 163)
+                main_tournoi.tournees.append(new_tour)
+                Tour(id=id_round, liste_de_matches=new_matches).saisir_score(main_tournoi, bdd)
+            else:
+                self.ajouter_tour_suivant(main_tournoi, bdd, id_round, new_matches)
+        except KeyboardInterrupt:
+            print(" ==> Ajout du tour annulé")
+            print("-" * 163)
+            return None
+
+    def ajouter_tour_suivant(self, main_tournoi, bdd, id_round, new_matches):
+        try:
+            """already_played = {
+                player1: [player2, player5],
+                player2: [player1, player6],
+                ...
+            }"""
+            already_played = {player: [] for player in main_tournoi.joueurs}
+            for tour in main_tournoi.tournees:
+                for match in tour.liste_de_matches:
+                    playerA = match.liste_de_joueurs[0]
+                    playerB = match.liste_de_joueurs[1]
+                    already_played[playerA].append(playerB)
+                    already_played[playerB].append(playerA)
+            players_added_to_round = []
+            i = 1
+            for player in main_tournoi.joueurs:
+                if player not in players_added_to_round:
+                    for adversaire in main_tournoi.joueurs:
+                        if (
+                                adversaire not in players_added_to_round
+                                and player != adversaire
+                                and adversaire not in already_played[player]
+                        ):
+                            id_match = f"{id_round}_match_{i}"
+                            i += 1
+                            new_matches.append(
+                                versus.Match(id=id_match, liste_de_joueurs=[player, adversaire]))
+                            players_added_to_round.append(adversaire)
+                            players_added_to_round.append(player)
+                            break
+            remaining_players = [player for player in main_tournoi.joueurs if player not in players_added_to_round]
+            j = len(remaining_players)
+            if j % 2 != 0:
+                raise Exception("uneven remaining players")
+            for idx in range(0, j, 2):
+                id_match = f"{id_round}_match_{i}"
+                i += 1
+                new_matches.append(
+                    versus.Match(id=id_match, liste_de_joueurs=[remaining_players[idx], remaining_players[idx+1]]))
+            new_tour = Tour(id=id_round, liste_de_matches=new_matches)
+            print("Nouveau round créé")
+            print("-" * 163)
+            print("Voici la liste des matches à jouer")
+            for i, match in enumerate(new_matches):
+                print(f"{i}/ {match.liste_de_joueurs[0].prenom} vs {match.liste_de_joueurs[1].prenom}")
+            print("-" * 163)
+            main_tournoi.tournees.append(new_tour)
+            Tour(id=id_round, liste_de_matches=new_matches).saisir_score(main_tournoi, bdd)
+        except KeyboardInterrupt:
+            print(" ==> Ajout du tour annulé")
+            print("-" * 163)
+            return None
+
+    def saisir_score(self, main_tournoi, bdd):
+        """
+        manages the input of the score for the ended matches into the round being managed
+        :param matchs_confirmes: from versus.py.py, ajouter_match, for it to be appended in round matches list
+        :param boucle: from versus.py.py, ajouter_match, for using of loop in this function
+        :param tour_termine: from versus.py.py, ajouter_match, for management of created tour since code in tour.py
+        :param main_tournoi: from versus.py.py, ajouter_match, for appending of round in the tournament rounds list
+        :return: None
+        """
+        try:
+            if not main_tournoi.tournees:
+                print("Round non créé, créez-en un nouveau")
+                Tour(f"tour_{len(main_tournoi.tournees) + 1}", "liste_de_matchs").ajouter_tour(main_tournoi, bdd)
+            round_to_modify = main_tournoi.tournees[-1]
+            if Tour(f"tour_{len(main_tournoi.tournees) + 1}", "liste_de_matchs").completed(main_tournoi):
+                print("Round terminé, créez-en un nouveau")
+                settings_2.modify_round(round_to_modify, main_tournoi, bdd)
+            for i, match in enumerate(round_to_modify.liste_de_matches):
+                print(f"{i}/ {match.liste_de_joueurs[0].prenom} vs {match.liste_de_joueurs[1].prenom}")
+            choix = int(input("Quel match voulez vous saisir ? "))
+            print("-" * 163)
+            match_to_modify = round_to_modify.liste_de_matches[choix]
+            if match_to_modify.resultat is not None:
+                choix_ter = "oui"
+                while choix_ter not in {"o", "n"}:
+                    choix_ter = input("Match déjà saisi, voulez-vous le modifier, oui (o) ou non (n) ? ")
+                    print("-" * 163)
+                    if choix_ter == "o":
+                        if match_to_modify.resultat == 0:
+                            match_to_modify.liste_de_joueurs[0].score -= 1
+                        elif match_to_modify.resultat == 1:
+                            match_to_modify.liste_de_joueurs[0].score -= 0.5
+                            match_to_modify.liste_de_joueurs[1].score -= 0.5
+                        elif match_to_modify.resultat == 2:
+                            match_to_modify.liste_de_joueurs[1].score -= 1
+                        choix_quater = int(input(f"Quel est le score du match (0 si "
+                                                 f"{match_to_modify.liste_de_joueurs[0].prenom} a gagné, "
+                                                 f"1 si nul, 2 si {match_to_modify.liste_de_joueurs[1].prenom}) ? "))
+                        print("-" * 163)
+                        if choix_quater == 0:
+                            match_to_modify.liste_de_joueurs[0].score += 1
+                        elif choix_quater == 1:
+                            match_to_modify.liste_de_joueurs[0].score += 0.5
+                            match_to_modify.liste_de_joueurs[1].score += 0.5
+                        elif choix_quater == 2:
+                            match_to_modify.liste_de_joueurs[1].score += 1
+                        match_to_modify.resultat = choix_quater
+                        print("Score du match saisi")
+                    elif choix_ter == "n":
+                        continue
+                self.saisir_score(main_tournoi, bdd)
+            else:
+                choix_bis = int(input(f"Quel est le score du match (0 si "
+                                      f"{match_to_modify.liste_de_joueurs[0].prenom} a gagné, "
+                                      f"1 si nul, 2 si {match_to_modify.liste_de_joueurs[1].prenom}) ? "))
+                print("-" * 163)
+                if choix_bis == 0:
+                    match_to_modify.liste_de_joueurs[0].score += 1
+                elif choix_bis == 1:
+                    match_to_modify.liste_de_joueurs[0].score += 0.5
+                    match_to_modify.liste_de_joueurs[1].score += 0.5
+                elif choix_bis == 2:
+                    match_to_modify.liste_de_joueurs[1].score += 1
+                match_to_modify.resultat = choix_bis
+                print("Score du match saisi")
+        except (ValueError, IndexError):
+            print("Je n'ai pas compris votre choix")
+            print("-" * 163)
+            self.saisir_score(main_tournoi, bdd)
+        except KeyboardInterrupt:
+            print(" ==> Modification du tour annulée")
+            print("-" * 163)
+            return None
+        self.saisir_score(main_tournoi, bdd)
---
+++ z__mvc__z/tournament.py
@@ -0,0 +1,173 @@
+from z__mvc__z import settings_2
+
+
+class Tournoi:
+    def __init__(self, id, nom, lieu, date,
+                 nombre_de_tours, tournees, joueurs,
+                 controle_du_temps, description):
+        self.id = id
+        self.nom = nom
+        self.lieu = lieu
+        self.date = date
+        self.nombre_de_tours = nombre_de_tours
+        self.tournees = tournees
+        self.joueurs = joueurs
+        self.controle_du_temps = controle_du_temps
+        self.description = description
+
+    def ajouter_tournoi_view(self, tous_les_tournois):
+        """
+        creates tournament instances, from the class specified above, whose name is 'Tournoi'
+        :param tous_les_tournois: from main.py, main_controller, to append all_tournaments list
+        :param bdd: from main.py, to create serialized tournament instances in json file
+        :return: for main.py, main_controller use, in case of adding or adding canceled
+        """
+        new_tournament = {
+            "id": f"tournoi_{len(tous_les_tournois)}",
+            "nom": "",
+            "lieu": "",
+            "date": "99-99-9999",
+            "nombre_de_tours": 4,
+            "tournees": [],
+            "joueurs": [],
+            "controle_du_temps": "quickblitz",
+            "description": ""
+        }
+        try:
+            while not new_tournament["nom"].isalpha():
+                new_tournament["nom"] = input("Entrez le nom du tournoi: ")
+            while not new_tournament["lieu"].isalpha():
+                new_tournament["lieu"] = input("Entrez le lieu du tournoi: ")
+            while not settings_2.validate(new_tournament["date"]):
+                new_tournament["date"] = input("Entrez la date du tournoi, JJ-MM-AAAA: ")
+            while new_tournament["controle_du_temps"].lower() not in {"bullet", "blitz", "quickshot"}:
+                new_tournament["controle_du_temps"] = input("Entrez le contrôle du temps du tournoi, "
+                                                            "bullet, blitz ou quickshot: ")
+            while not new_tournament["description"].isalpha():
+                new_tournament["description"] = input("Entrez la description du tournoi: ")
+            print("-" * 163)
+            return new_tournament
+        except KeyboardInterrupt:
+            print(" ==> Ajout du tournoi annulé")
+            print("-" * 163)
+
+    def ajouter_tournoi_controller(self, tous_les_tournois, bdd):
+        """
+        creates tournament instances, from the class specified above, whose name is 'Tournoi'
+        :param tous_les_tournois: from main.py, main_controller, to append all_tournaments list
+        :param bdd: from main.py, to create serialized tournament instances in json file
+        :return: for main.py, main_controller use, in case of adding or adding canceled
+        """
+        try:
+            new_tournament_data = self.ajouter_tournoi_view(tous_les_tournois)
+            new_tournament = Tournoi(new_tournament_data["id"], new_tournament_data["nom"],
+                                     new_tournament_data["lieu"], new_tournament_data["date"],
+                                     new_tournament_data["nombre_de_tours"], new_tournament_data["tournees"],
+                                     new_tournament_data["joueurs"], new_tournament_data["controle_du_temps"],
+                                     new_tournament_data["description"])
+            tous_les_tournois.append(new_tournament)
+            settings_2.tournament_encode(new_tournament, bdd)
+        except KeyboardInterrupt:
+            print(" ==> Ajout du tournoi annulé")
+            print("-" * 163)
+
+    def editer_tournoi_view(self, tous_les_tournois):
+        """
+        edit created tournament instances
+        :param tous_les_tournois: from main.py, main_controller, to edit all_tournaments list
+        :param bdd: from main.py, to update serialized tournament instances in json file
+        :return: None
+        """
+        new_data_tournament = {
+            "nom": "",
+            "lieu": "",
+            "date": "99-99-9999",
+            "controle_du_temps": "quickblitz",
+            "description": ""
+        }
+        try:
+            for i, tournament in enumerate(tous_les_tournois):
+                print(f"{i}/ {tournament.nom}")
+            index = int(input("Quel tournoi voulez-vous modifier ? "))
+            print("-" * 163)
+            tournament_to_modify = tous_les_tournois[index]
+            print(f"0/ Nom: {tournament_to_modify.nom}")
+            print(f"1/ Lieu: {tournament_to_modify.lieu}")
+            print(f"2/ Date: {tournament_to_modify.date}")
+            print(f"3/ Temps: {tournament_to_modify.controle_du_temps}")
+            print(f"4/ Description: {tournament_to_modify.description}")
+            print("5/ Annulation et retour")
+            choix = input("Que voulez-vous modifier ? ")
+            print("-" * 163)
+            if choix in {"0", "1", "2", "3", "4", "5"}:
+                return new_data_tournament, tournament_to_modify, choix
+            else:
+                print("Je n'ai pas compris votre choix")
+                print("-" * 163)
+                self.editer_tournoi_view(tous_les_tournois)
+            print("-" * 163)
+        except (ValueError, IndexError):
+            print("Je n'ai pas compris votre choix")
+            print("-" * 163)
+            self.editer_tournoi_view(tous_les_tournois)
+        except KeyboardInterrupt:
+            print(" ==> Modification du tournoi annulée")
+            print("-" * 163)
+            return None
+
+    def editer_tournoi_controller(self, tous_les_tournois, bdd):
+        """
+        edit created tournament instances
+        :param tous_les_tournois: from main.py, main_controller, to edit all_tournaments list
+        :param bdd: from main.py, to update serialized tournament instances in json file
+        :return: None
+        """
+        new_data_tournament, tournament_to_modify, choix = self.editer_tournoi_view(tous_les_tournois)
+        if len(tous_les_tournois) > 0:
+            try:
+                if choix == "0":
+                    while not new_data_tournament["nom"].isalpha():
+                        new_data_tournament["nom"] = input("Entrer le nouveau nom du tournoi: ")
+                    tournament_to_modify.nom = new_data_tournament["nom"]
+                    print("Le tournoi a été modifié")
+                elif choix == "1":
+                    while not new_data_tournament["lieu"].isalpha():
+                        new_data_tournament["lieu"] = input("Entrer le nouveau lieu du tournoi: ")
+                    tournament_to_modify.lieu = new_data_tournament["lieu"]
+                    print("Le tournoi a été modifié")
+                elif choix == "2":
+                    while not settings_2.validate(new_data_tournament["date"]):
+                        new_data_tournament["date"] = input("Entrer la nouvelle date du tournoi: ")
+                    tournament_to_modify.date = new_data_tournament["date"]
+                    print("Le tournoi a été modifié")
+                elif choix == "3":
+                    while new_data_tournament["controle_du_temps"].lower() not in {"bullet", "blitz", "quickshot"}:
+                        new_data_tournament["controle_du_temps"] = input("Entrer le nouveau "
+                                                                         "contrôle du temps du tournoi: ")
+                    tournament_to_modify.controle_du_temps = new_data_tournament["nom"]
+                    print("Le tournoi a été modifié")
+                elif choix == "4":
+                    while not new_data_tournament["description"].isalpha():
+                        new_data_tournament["nom"] = input("Entrer la nouvelle description du tournoi: ")
+                    tournament_to_modify.description = new_data_tournament["description"]
+                    print("Le tournoi a été modifié")
+                elif choix == "5":
+                    return None
+                else:
+                    print("Je n'ai pas compris votre choix")
+                    print("-" * 163)
+                    self.editer_tournoi_controller(tous_les_tournois, bdd)
+                print("-" * 163)
+                settings_2.edit_tournament_encode(tournament_to_modify, bdd)
+            except (ValueError, IndexError):
+                print("Je n'ai pas compris votre choix")
+                print("-" * 163)
+                self.editer_tournoi_controller(tous_les_tournois, bdd)
+            except KeyboardInterrupt:
+                print(" ==> Modification du tournoi annulée")
+                print("-" * 163)
+                return None
+        else:
+            print("Aucun tournoi n'a déjà été ajouté")
+            print("-" * 163)
+            return None
